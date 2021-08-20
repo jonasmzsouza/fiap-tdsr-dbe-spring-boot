f87d5efa1867a4dedf900ffe5565d0569ff1ea81
Jonas Souza <jonasmzsouza@gmail.com>
2021-08-20 19:29:27 -0300

    Introdução ao Spring e Spring Data JPA

---
+++ epictask/src/main/java/br/com/fiap/epictask/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package br.com.fiap.epictask.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.epictask.model.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long>{
+
+	List<Task> findByTitle(String title);
+
+}
