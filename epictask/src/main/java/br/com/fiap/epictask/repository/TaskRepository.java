c0cf5b2578a18d2c018081ae8a5f08a0bcb4142d
Jonas Souza <jonasmzsouza@gmail.com>
2021-08-28 00:02:42 -0300

    API REST

--- epictask/src/main/java/br/com/fiap/epictask/repository/TaskRepository.java
+++ epictask/src/main/java/br/com/fiap/epictask/repository/TaskRepository.java
@@ -2,12 +2,14 @@ package br.com.fiap.epictask.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.epictask.model.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
-	List<Task> findByTitle(String title);
+	Page<Task> findByTitleLike(String title, Pageable pageable);
 
 }
